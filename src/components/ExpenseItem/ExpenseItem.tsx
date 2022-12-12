2089086e1f8bb089355c9257b3f377c870c723c3
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-12 19:45:44 -0300

    Added new updates

--- src/components/ExpenseItem/ExpenseItem.tsx
+++ src/components/ExpenseItem/ExpenseItem.tsx
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
+  const expenseDate = new Date(2022, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div> {expenseDate.toISOString()} </div>
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2> {expenseTitle}</h2>
+        <div className="expense-item__price"> ${expenseAmount} </div>
       </div>
     </div>
   );
