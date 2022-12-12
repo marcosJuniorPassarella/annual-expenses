5b43144b0246da85ce251f47a0256de50218a1ea
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-12 19:38:54 -0300

    Added styles

--- src/components/ExpenseItem/ExpenseItem.tsx
+++ src/components/ExpenseItem/ExpenseItem.tsx
@@ -1,9 +1,13 @@
+import "./ExpenseItem.css";
+
 export default function ExpenseItem() {
   return (
-    <>
+    <div className="expense-item">
       <div>Date</div>
-      <div>Title</div>
-      <div>Amount</div>
-    </>
+      <div className="expense-item__description">
+        <h2>Car insurance</h2>
+        <div className="expense-item__price">Amount</div>
+      </div>
+    </div>
   );
 }
