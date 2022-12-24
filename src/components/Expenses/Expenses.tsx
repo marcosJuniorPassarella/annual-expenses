6eff1b821fcb55d5dca5a6a97aa45db3c89639a4
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-24 15:03:37 -0300

    Added new component

---
+++ src/components/Expenses/Expenses.tsx
@@ -0,0 +1,38 @@
+import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import './Expenses.css';
+
+interface ExpensesProps {
+  expenses: {
+    id: string;
+    title: string;
+    amount: number;
+    date: Date;
+  }[];
+}
+
+export default function Expenses({ expenses }: ExpensesProps) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        date={expenses[0].date}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        date={expenses[1].date}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        date={expenses[2].date}
+        amount={expenses[2].amount}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        date={expenses[3].date}
+        amount={expenses[3].amount}
+      />
+    </div>
+  );
+}
