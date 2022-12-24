19b854bc4ae185b3fcdd9585ef6b818df901e725
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-24 14:03:47 -0300

    Added props

--- src/components/ExpenseItem/ExpenseItem.tsx
+++ src/components/ExpenseItem/ExpenseItem.tsx
@@ -1,16 +1,18 @@
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
-  const expenseDate = new Date(2022, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+interface ExpenseItemProps {
+  title: string;
+  date: Date;
+  amount: number;
+}
 
+export default function ExpenseItem({ title, date, amount }: ExpenseItemProps) {
   return (
     <div className="expense-item">
-      <div> {expenseDate.toISOString()} </div>
+      <div> {date.toISOString()} </div>
       <div className="expense-item__description">
-        <h2> {expenseTitle}</h2>
-        <div className="expense-item__price"> ${expenseAmount} </div>
+        <h2> {title}</h2>
+        <div className="expense-item__price"> ${amount} </div>
       </div>
     </div>
   );
