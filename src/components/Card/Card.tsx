29166166f95b9ac939c26763611e3409ed44d736
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-30 09:53:19 -0300

    Receive external css classes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,9 @@
 import "./Card.css";
 
 export default function Card(props: any) {
+  // recebendo classes de estilo que forem passadas externamente
+  const classes = `card ${props.className}`;
   // ao passar props.children como abaixo, é possível
   // renderizar o conteúdo que for colocado entre chaves no template
-  return <div className="card"> {props.children} </div>;
+  return <div className={classes}> {props.children} </div>;
 }
