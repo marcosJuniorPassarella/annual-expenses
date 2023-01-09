bb3cd9e7d856956afd8f8d58e9cdb04a7ec37b52
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-09 18:38:34 -0300

    Implement useState hook

--- src/components/Expenses/ExpenseItem/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem/ExpenseItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "../../Expenses/ExpenseDate/ExpenseDate";
 import Card from "../../UI/Card/Card";
@@ -9,13 +10,20 @@ interface ExpenseItemProps {
 }
 
 export default function ExpenseItem({ title, date, amount }: ExpenseItemProps) {
+  const [modifiedTitle, setModifiedTitle] = useState(title);
+
+  const clickHandler = () => {
+    setModifiedTitle("JETTA");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{modifiedTitle}</h2>
         <div className="expense-item__price"> ${amount} </div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
