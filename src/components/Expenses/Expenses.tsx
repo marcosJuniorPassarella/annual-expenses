52a17e54ee4b23b164c8f6b08817986002b2e3d6
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-08 19:34:27 -0300

    Implement two-way-databinding in ExpensesFilter component

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Card from "../UI/Card/Card";
 import ExpenseItem from "../Expenses/ExpenseItem/ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
 
 interface ExpensesProps {
   expenses: {
@@ -12,28 +14,41 @@ interface ExpensesProps {
 }
 
 export default function Expenses({ expenses }: ExpensesProps) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear: string) => {
+    console.log("Expenses", selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      />
-    </Card>
+    <div className="">
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          title={expenses[0].title}
+          date={expenses[0].date}
+          amount={expenses[0].amount}
+        />
+        <ExpenseItem
+          title={expenses[1].title}
+          date={expenses[1].date}
+          amount={expenses[1].amount}
+        />
+        <ExpenseItem
+          title={expenses[2].title}
+          date={expenses[2].date}
+          amount={expenses[2].amount}
+        />
+        <ExpenseItem
+          title={expenses[3].title}
+          date={expenses[3].date}
+          amount={expenses[3].amount}
+        />
+      </Card>
+    </div>
   );
 }
