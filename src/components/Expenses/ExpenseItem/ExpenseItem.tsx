918614b9b317577b33cf0ebc6c1facb49be871fc
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-08 19:52:20 -0300

    Added updates in the ExpenseItem component

--- src/components/Expenses/ExpenseItem/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem/ExpenseItem.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "../../Expenses/ExpenseDate/ExpenseDate";
 import Card from "../../UI/Card/Card";
@@ -10,20 +9,13 @@ interface ExpenseItemProps {
 }
 
 export default function ExpenseItem({ title, date, amount }: ExpenseItemProps) {
-  const [modifiedTitle, setModifiedTitle] = useState(title);
-
-  const clickHandler = () => {
-    setModifiedTitle("JETTA");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{modifiedTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price"> ${amount} </div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
