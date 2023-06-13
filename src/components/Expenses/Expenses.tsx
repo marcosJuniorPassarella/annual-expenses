48b0e9afbf3981c266caceec7000de72372b29ba
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-06-13 20:08:03 -0300

    Render expenses list

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -28,26 +28,14 @@ export default function Expenses({ expenses }: ExpensesProps) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={expenses[0].title}
-          date={expenses[0].date}
-          amount={expenses[0].amount}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          date={expenses[1].date}
-          amount={expenses[1].amount}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          date={expenses[2].date}
-          amount={expenses[2].amount}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          date={expenses[3].date}
-          amount={expenses[3].amount}
-        />
+        {expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        ))}
       </Card>
     </div>
   );
