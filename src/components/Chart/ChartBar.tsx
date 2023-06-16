7efb7b76117e279762c485a760d4855cbd36d476
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-06-16 19:51:36 -0300

    Implement dynamic styles

--- src/components/Chart/ChartBar.tsx
+++ src/components/Chart/ChartBar.tsx
@@ -1,4 +1,29 @@
-const ChartBar = (propos: any) => {
-  return <></>;
+import "./ChartBar.css";
+
+interface ChartBar {
+  key: string;
+  value: any;
+  maxValue: any;
+  label: string;
+}
+
+const ChartBar = (props: ChartBar) => {
+  let barFillHeight = "0%";
+
+  props.maxValue > 0 &&
+    (barFillHeight =
+      String(Math.round(props.value / props.maxValue) * 100) + "%");
+
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
 };
 export default ChartBar;
